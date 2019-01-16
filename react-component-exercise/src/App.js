7470f0297233e8d121a813a0f5587212871a2358
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-01-16 15:52:55 -0800

    added navbar and routing

--- react-component-exercise/src/App.js
+++ react-component-exercise/src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import Navbar from "./components/navbar";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <Movies />;
+    return (
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/notFound" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/notFound" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
